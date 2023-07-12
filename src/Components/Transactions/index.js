d919c4b718d6c34d41ee523b7c6615dd0d51c224
Harshith Diyyala <125182850+harshithdiyyala@users.noreply.github.com>
2023-07-12 12:29:34 +0530

    Update index.js

--- src/Components/Transactions/index.js
+++ src/Components/Transactions/index.js
@@ -26,7 +26,7 @@ class Transactions extends Component {
                 headers :{
                     "Content-Type": "application/json",
                     "x-hasura-admin-secret":"g08A3qQy00y8yFDq3y6N1ZQnhOPOa4msdie5EtKS1hFStar01JzPKrtKEzYY2BtF",
-                    "x-hasura-role":"user",
+                    "x-hasura-role":{token ==='3' ? "admin":"user"},
                     "x-hasura-user-id":token
                 }
             }
